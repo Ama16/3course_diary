6e65cd697c906c14c91d7bcf2b2d9c7850ed7c97
Artyom Makhin <48079881+Ama16@users.noreply.github.com>
2020-11-08 18:47:32 +0300

    Create PR.py

---
+++ category_encoders/PR.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class PREncoder(BaseEstimator, TransformerMixin):
+    """Probability Ratio Encoding для категориальных данных.
+         Только для бинарного таргета.
+    
+    Параметры
+    ----------
+    
+    col: list
+        название колонки, которую необходимо преобразовать
+    unknown: float
+        если значение категориального признака впервые встретилось в преобразовываемой колонкe, помещается unknown. При None выбрасывается исключение
+        
+    """
+    def __init__(self, col=None, unknown=None):
+        self.col = col
+        self.unknown = unknown
+        
+    def fit(self, X, y): 
+        col = X[self.col].reset_index(drop=True)
+        y = y.reset_index(drop=True)
+        
+        
+        data = pd.DataFrame(pd.concat([col, y], axis=1))
+        name = data.columns[1]
+        tmp = pd.DataFrame(data.groupby(col)[name].count())[name]
+        data = pd.DataFrame(data.groupby(col)[name].sum())
+        data['not_target'] = tmp - data[name]
+        data['not_target'] = np.where(data['not_target'] == 0, 0.000001, data['not_target'])
+        data['answer'] = data[name]/ data['not_target']
+        self.d = dict(data['answer'])
+        
+        return self
+    
+    def transform(self, X):
+        col = X[self.col]
+        answer = np.empty(len(col))
+        for k, i in enumerate(X.index):
+            if col[i] in self.d.keys():
+                answer[k] = self.d[col[i]]
+            else:
+                if self.unknown == None:
+                    raise Exception("Unexpected value")
+                else:
+                    answer[k] = self.unknown
+        return pd.Series(answer, index=col.index)
